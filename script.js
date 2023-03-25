7790ba14fc2ec8aecafc28d798691bd6894e7853
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2023-03-25 08:21:03 +0000

    Add site slogan on landing page

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianRtY29kZXIiLCJhIjoiY2xmbW15cmt5MGRsMjQ0cjJxYTV2OXk4ZSJ9.AAwKG-onfL-5lPLHpEW4RQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-6.266155, 53.350140],
+    zoom: 12,
+});
